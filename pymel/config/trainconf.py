1b91ab819312d7b45ff1f15b5e9099145126b66c
KhoiDOO <khoido8899@gmail.com>
2023-08-19 11:24:27 +0700

    remove attr model and method

--- pymel/config/trainconf.py
+++ pymel/config/trainconf.py
@@ -5,23 +5,13 @@ from torch import nn
 
 class TrainConfig:
     def __init__(self,
-                 model: nn.Module = None,
                  checkpoint: bool = True,
                  logging: bool = True,
                  save_dir: str = None,
-                #  method: str = None,
                  save_best: bool = True,
                  extension:str = "pt",
                  save_last: bool = False
                  ) -> None:
-        
-        if model is None:
-            raise ValueError("model cannot be None")    
-        elif not isinstance(model, nn.Module):
-            raise TypeError(f"model must be a nn.Module type, \
-                but found {type(model)} instead")
-        else:
-            self.model = model
         
         for _varn, _var in zip(["checkpoint", "logging"], [checkpoint, logging]):
             if not isinstance(_var, bool):
@@ -30,21 +20,11 @@ class TrainConfig:
         self.cp = checkpoint
         self.lg = logging
         
-        # if method is None:
-        #     raise ValueError("method cannot be None")
-        # elif not isinstance(method, str):
-        #     raise TypeError(f"method must be a string, \
-        #         but found {type(method)} instead")
-        # else:
-        #     self.method = method
-            
-        
         if self.cp or self.lg:
             if save_dir is None:
                 self.sv_dir = os.getcwd() + "/pymel_benchmark"
                 if not os.path.exists(self.sv_dir):
                     os.mkdir(self.sv_dir)
-                self.__folder_setup()
             else:
                 if not isinstance(save_dir, str):
                     raise TypeError(f"save_dir must be a string \
@@ -55,17 +35,16 @@ class TrainConfig:
                 else:
                     self.sv_dir = save_dir + "/pymel_benchmark"
                     if not os.path.exists(self.sv_dir):
-                        os.mkdir(self.sv_dir)
-                    self.__folder_setup()
+                        os.mkdir(self.sv_dir)                    
         
         if self.cp:
             self.sv_best = save_best
             self.sv_last = save_last
             self.ext = extension
             
-        
-    def __folder_setup(self):
-        self.method_dir = self.sv_dir + f"/{self.method}"
+    @staticmethod
+    def folder_setup(self, method):
+        self.method_dir = self.sv_dir + f"/{method}"
         if not os.path.exists(self.method_dir):
             os.mkdir(self.method_dir)
         sub_dirs = os.listdir(self.method_dir)
