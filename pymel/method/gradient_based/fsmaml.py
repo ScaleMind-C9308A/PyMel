86f922b112649f94bad5b5c41e18c09f699b9d55
KhoiDOO <khoido8899@gmail.com>
2023-08-21 23:32:14 +0700

    update functional api for fsmaml

--- pymel/method/gradient_based/fsmaml.py
+++ pymel/method/gradient_based/fsmaml.py
@@ -213,3 +213,164 @@ class FSMAML(Trainer):
                 print(f"Epoch: {epoch} - MetaLoss: {metaloss/num_task} - Test Loss: {test_loss/batch_count} - Test Acc: {100*correct/total}%")  
     
         dist.destroy_process_group()
+
+
+def fsmaml(
+    ds_cfg: DSConfig, tr_cfg: TrainConfig, 
+    model: nn.Module = None, gpus: List[int] = ...,
+    meta_opt: str = None, 
+    meta_lr: float = 0.001,
+    meta_wd: float = 1e-4,
+    sp_opt: str = None,
+    sp_lr: float = 0.01,
+    sp_wd: float = 1e-4,
+    criterion: torch.nn.Module = nn.CrossEntropyLoss(),
+    outer_epoch: int = 100,
+    inner_epoch: int = 1
+):
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+        
+    args.rank = 0
+    port = 8080
+    args.dist_url = f'tcp://localhost:{port}'
+    print(f"PyMel GPT: The experiment is deployed at {args.dist_url}")
+    
+    args.world_size = torch.cuda.device_count()
+    
+    args.ds_cfg = ds_cfg
+    args.tr_cfg = tr_cfg
+    args.model = model
+    args.gpus = gpus
+    args.meta_opt = meta_opt
+    args.sp_opt = sp_opt
+    args.meta_lr = meta_lr
+    args.meta_wd = meta_wd
+    args.sp_lr = sp_lr
+    args.sp_wd = sp_wd
+    args.crit = criterion        
+    args.outer_epoch = outer_epoch
+    args.inner_epoch = inner_epoch
+    
+    mp.spawn(main_worker, (args,), nprocs = args.world_size)
+
+def main_worker(gpu, args):
+    print("get here")
+    args.rank += gpu
+
+    dist.init_process_group(
+        backend='nccl', 
+        init_method=args.dist_url,
+        world_size=args.world_size, rank=args.rank)
+    
+    torch.cuda.set_device(gpu)
+    torch.backends.cudnn.benchmark = True
+    print("Process Group Init: Done")
+    
+    batch_size = args.ds_cfg.get_k_shot() + args.ds_cfg.get_k_query()
+    assert batch_size % args.world_size == 0
+    assert args.ds_cfg.get_k_shot() % args.world_size == 0
+    assert args.ds_cfg.get_k_query() % args.world_size == 0
+    
+    train_sampler = DistributedSampler(args.ds_cfg.train_ds)
+    test_sampler = DistributedSampler(args.ds_cfg.test_ds)
+    
+    per_device_batch_size = batch_size // args.world_size
+    per_device_k_shot = args.ds_cfg.get_k_shot() // args.world_size
+    per_device_k_query = args.ds_cfg.get_k_query() // args.world_size
+    
+    train_dl = DataLoader(
+        dataset=args.ds_cfg.train_ds, 
+        batch_size=per_device_batch_size, 
+        num_workers=args.ds_cfg.get_wk(), 
+        pin_memory=args.ds_cfg.get_pin_mem(), 
+        sampler=train_sampler
+    )
+    
+    test_dl = DataLoader(
+        dataset=args.ds_cfg.test_ds,
+        batch_size=1,
+        num_workers=args.ds_cfg.get_wk(), 
+        pin_memory=args.ds_cfg.get_pin_mem(),
+        sampler=test_sampler
+    )
+    print("Data Loader Setup: Done")
+    
+    global_model = args.model.cuda(gpu)
+    global_model = nn.SyncBatchNorm.convert_sync_batchnorm(global_model)
+    global_model = torch.compile(model=global_model)
+    global_model = DDP(global_model, device_ids=[gpu])
+    
+    print("Setup Model: Done")
+    
+    meta_optimizer = opt_mapping[args.meta_opt](
+        global_model.parameters(), 
+        lr=args.meta_lr, weight_decay=args.meta_wd
+    )
+    
+    num_task = args.ds_cfg.train_ds.nt
+    for epoch in range(args.outer_epoch):
+        train_sampler.set_epoch(epoch)
+        global_model.train()
+        
+        for train_idx, data_dict in enumerate(train_dl):
+            
+            metaloss = 0.0
+            for task in data_dict:
+                task_model = copy.deepcopy(global_model)
+                task_optimizer = opt_mapping[args.meta_opt](
+                    task_model.parameters(), 
+                    lr=args.sp_lr, weight_decay=args.sp_wd
+                )
+                
+                sp_x, sp_y, qr_x, qr_y = single_task_detach(
+                    batch_dict=data_dict,
+                    k_shot=per_device_k_shot,
+                    k_query=per_device_k_query,
+                    task=task
+                )
+                
+                for in_e in range(args.inner_epochs):
+                    sp_x, sp_y = sp_x.cuda(gpu), sp_y.cuda(gpu)
+                    sp_logits = task_model(sp_x)
+                    sp_loss = args.crit(sp_logits, sp_y)
+                    task_optimizer.zero_grad()
+                    sp_loss.backward()
+                    task_optimizer.step()
+                
+                qr_x, qr_y = qr_x.cuda(gpu), qr_y.cuda(gpu)
+                qr_logits = task_model(qr_x)
+                qr_loss = args.crit(qr_logits, qr_y)
+                metaloss += qr_loss.item()
+                qr_loss.backward()            
+
+                for w_global, w_local in zip(global_model.parameters(), task_model.parameters()):
+                    if w_global.grad is None:
+                        w_global.grad = w_local.grad
+                    else:
+                        w_global.grad += w_local.grad
+
+            meta_optimizer.step()
+            meta_optimizer.zero_grad()
+        if args.rank == 0:
+            test_sampler.set_epoch(epoch)
+            global_model.eval()
+            with torch.no_grad():
+                test_loss = 0
+                correct = 0
+                total = 0
+                batch_count = 0
+                for test_idx, (test_imgs, test_labels) in enumerate(test_dl):
+                    batch_count = test_idx
+                    test_imgs = test_imgs.cuda(gpu)
+                    test_labels = test_labels.cuda(gpu)
+                    test_logits = global_model(test_imgs)                
+                
+                    test_loss += args.crit(test_logits, test_labels).item()
+                    _, predicted = test_logits.max(1)
+                    total += test_labels.size(0)
+                    correct += predicted.eq(test_labels).sum().item()
+                    
+            print(f"Epoch: {epoch} - MetaLoss: {metaloss/num_task} - Test Loss: {test_loss/batch_count} - Test Acc: {100*correct/total}%")  
+
+    dist.destroy_process_group()
